1e2abb13247ff788ee378d16298500cafd1b0c2d
T.J. Ruggles <t.ruggles@live.ca>
2020-08-11 23:28:40 -0400

    initial logic for number, operator, and decimal keys for calculator's display

--- scripts.js
+++ scripts.js
@@ -1,12 +1,28 @@
 const calculator = document.querySelector(".calculator")
 const keys = document.querySelector(".calculator__keys")
+const display = document.querySelector(".calculator__display")
 
 keys.addEventListener("click", (e) => {
     if (e.target.matches("button")) {
         const key = e.target
         const action = key.dataset.action
+        const keyContent = key.textContent
+        const displayedNum = display.textContent
+
+        //Remove 'is-depressed' class from all operator keys after number key is pressed
+        Array.from(key.parentNode.children).forEach((k) =>
+            k.classList.remove("is-depressed")
+        )
 
         if (!action) {
+            //replace default 0 in calc with number pressed
+            if (displayedNum === 0 || previousKeyType === "operator") {
+                display.textContent === keyContent
+                //add key pressed to number already in display
+            } else {
+                display.textContent = displayedNum + keyContent
+            }
+
             console.log("number key")
         }
 
@@ -16,10 +32,16 @@ keys.addEventListener("click", (e) => {
             action === "multiply" ||
             action === "divide"
         ) {
+            //to know which operator is active
+            key.classList.add("is-depressed")
+            //add custom attribute
+            calculator.dataset.previousKeyType = "operator"
             console.log("operator key")
         }
 
         if (action === "decimal") {
+            display.textContent = displayedNum + "."
+
             console.log("decimal key")
         }
 
